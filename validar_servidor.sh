5372a2b1161dffcf4cb325c8f5071ee98ee548de
Daniel Coutinho <143365717+DanielCoutinh0@users.noreply.github.com>
2024-10-30 21:41:34 -0300

    validar_servidor.sh

--- validar_servidor.sh
+++ validar_servidor.sh
@@ -1,29 +1,28 @@
 #Fora do Script
-1. Criar o arquivo de validação
-sudo nano /usr/local/bin/validar_nginx_status.sh
+1. Criar um diretorio no /var/log/
+rmdir nginx_validacao
+
+2. Criar o arquivo de validação
+sudo nano /usr/local/bin/validar_servidor.sh
 
 #Dentro do Script
-2. Diretorio para salvar os logs
-LOG_DIR="/var/log/nginx_validation"
-
 3. Arquivos de saída para online e offline
-LOG_ONLINE="$LOG_DIR/nginx_online.log"
-LOG_OFFLINE="$LOG_DIR/nginx_offline.log"
+DIRETORIO_ONLINE="/var/log/nginx_validacao/nginx_online.log"
+LOG_OFFLINE="/var/log/nginx_validacao/nginx_offline.log"
 
-4. Verificar se o diretório de logs existe, se não, cria
-if [ ! -d "$LOG_DIR" ]; then
-    mkdir -p "$LOG_DIR"
-fi
+4. Data e Hora
+DATA_HORA_ATUAL=$(date +'%Y-%m-%d %H:%M:%S')
 
-5. Data e Hora
-DATA_HORA=$(date +'%Y-%m-%d %H:%M:%S')
+5. Nome serviço
+NOME_SERVICO="Nginx"
 
-6. Nome serviço
-SERVICO="Nginx"
+6. Status
+STATUS_ONLINE="ONLINE"
+STATUS_OFFLINE="OFFLINE"
 
 7. Validar se o Nginx está rodando
 if systemctl is-active --quiet nginx; then
-    echo "$DATA_HORA - Serviço: $SERVICO - Status: ONLINE - O servidor esta rodando." >> "$LOG_ONLINE"
+    echo "$DATA_HORA_ATUAL - Serviço: $_NOME_SERVICO - Status: $STATUS_ONLINE - O servidor esta rodando." tee -a "$DIRETORIO_ONLINE" > /dev/null
 else
-    echo "$DATA_HORA - Serviço: $SERVICO - Status: OFFLINE - O servidor esta parado ou com problemas ." >> "$LOG_OFFLINE"
+    echo "$DATA_HORA_ATUAL - Serviço: $NOME_SERVICO - Status: $STATUS_OFFLINE - O servidor esta parado ou com problemas ." tee -a "$DIRETORIO_OFFLINE" > /dev/null
 fi
